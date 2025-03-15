033886f0d0e49c503201f94ff466c4be7eeaf8c5
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-03-15 14:02:17 +0530

    Adding the post page for seperate displaying

--- src/app/blog/posts/page.tsx
+++ src/app/blog/posts/page.tsx
@@ -1,17 +1,11 @@
-import { posts } from '@/app/lib/placeholder-data'
+import {posts} from '@/app/lib/placeholder-data'
+import Post from "@/app/ui/components/posts/Post";
 
-export default function Page(){
+export default function Page() {
     return (
         <>
             <h1>Posts</h1>
-            <ul>
-                {posts.map(post => (
-                    <li key={post.id}>
-                        <h2>{post.title}</h2>
-                        <p>{post.content}</p>
-                    </li>
-                ))}
-            </ul>
+            {posts.map((post) => <Post key={post.id} {...post} />)}
         </>
     )
 }
