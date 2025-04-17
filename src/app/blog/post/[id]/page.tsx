ffef09a4ea7f80860db64b28cbbb2f54be5f2b65
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-04-18 00:35:21 +0530

    corected the param error

--- src/app/blog/post/[id]/page.tsx
+++ src/app/blog/post/[id]/page.tsx
@@ -1,11 +1,16 @@
 import { posts } from '@/app/lib/placeholder-data';
 import Post from '@/app/ui/components/posts/Post';
 
-export default function Page({ params }: { params: { id: string } }) {
+interface PageProps {
+    params: { id: string }
+}
+
+export default function Page({ params }: PageProps) {
     const post = posts.find((post) => post.id === params.id);
     return (
         <>
             <h1 className={"text-sky-700"}>Post</h1>
             {post && <Post {...post} />}
-        </>)
+        </>
+    );
 }
