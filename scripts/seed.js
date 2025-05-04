1d0de20a568669986b11292e0b238bb049d06356
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-05-04 22:38:11 +0530

    Adding data to the database using seed

--- scripts/seed.js
+++ scripts/seed.js
@@ -4,30 +4,37 @@ const { posts } = require("../src/app/lib/placeholder-data.js");
 async function seedPosts(client) {
   try {
     await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-    // Create the "users" table if it doesn't exist
+
+    // Create the "posts" table if it doesn't exist
     const createTable = await client.sql`
       CREATE TABLE IF NOT EXISTS posts (
-        id UUID DEFAULT uuid_generate_v1mc() PRIMARY KEY,
+        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
         author VARCHAR(255) NOT NULL,
-        title VARCHAR(255) NOT NULL UNIQUE,
+        title VARCHAR(255) NOT NULL,
         content TEXT NOT NULL,
-        date TEXT NOT NULL
+        date TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
       );
     `;
 
     console.log(`Created "posts" table`);
 
-    // Insert data into the "users" table
+    // Insert data into the "posts" table
     const insertedPosts = await Promise.all(
-      posts.map(async (post) => {
-        return client.sql`
-        INSERT INTO posts (id, title, content, date, author)
-        VALUES (${post.id}, ${post.title}, ${post.content}, ${post.date}, ${post.user})
-        ON CONFLICT (id) DO NOTHING;
-      `;
-      })
+        posts.map(async (post) => {
+          return client.sql`
+            INSERT INTO posts (id, title, content, date, author)
+            VALUES (${post.id || null}, ${post.title}, 
+            ${post.content}, ${post.date}, ${post.user || post.author || 'Unknown Author'})
+            ON CONFLICT (id) DO UPDATE SET
+            title = ${post.title},
+            content = ${post.content},
+            date = ${post.date},
+            author = ${post.user || post.author || 'Unknown Author'};
+          `;
+        })
     );
-    console.log(`Seeded ${insertedPosts.length} posts articles`);
+
+    console.log(`Seeded ${insertedPosts.length} posts`);
 
     return {
       createTable,
@@ -40,14 +47,14 @@ async function seedPosts(client) {
 }
 
 async function main() {
-  const client = await db.connect();
-  await seedPosts(client);
-  await client.end();
+  try {
+    const client = await db.connect();
+    await seedPosts(client);
+    await client.end();
+    console.log("Database seeding completed successfully");
+  } catch (error) {
+    console.error("An error occurred while seeding the database:", error);
+  }
 }
 
-main().catch((err) => {
-  console.error(
-    "An error occurred while attempting to seed the database:",
-    err
-  );
-});
+main();
