d16999273149446cf391ce832186bbf0b7cc4b6d
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-05-05 22:58:19 +0530

    Adding Button for adding Posts

--- src/app/blog/posts/page.tsx
+++ src/app/blog/posts/page.tsx
@@ -1,6 +1,8 @@
 // import {posts} from '@/app/lib/placeholder-data';
 import Post from '@/app/ui/components/posts/Post';
 import { connectToDB, getPosts } from '@/app/lib/data';
+import Link from "next/link";
+import {Button} from "@/app/ui/components/button";
 
 export default async function Page() {
     const client = await connectToDB();
@@ -8,6 +10,7 @@ export default async function Page() {
     return (
         <>
             {client && <p className={'text-green-500'}>Connected to DB</p> }
+            <Link href="/blog/post/insert"><Button className="outline border-sky-700 text-sky-700 hover:bg-sky-700 hover:text-white my-5 py-2 px-4 rounded-xl">New +</Button></Link>
             <h1 className={'text-sky-700'}>Posts</h1>
             {posts?.map((post) => <Post key={post.id} {...post} />)}
         </>)
