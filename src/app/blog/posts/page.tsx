3e262e7ff2d748f3fb724fef0ce73ff111e024ee
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-05-05 16:23:50 +0530

    Fetching data from the database from sql using data.ts

--- src/app/blog/posts/page.tsx
+++ src/app/blog/posts/page.tsx
@@ -1,13 +1,14 @@
-import {posts} from '@/app/lib/placeholder-data';
+// import {posts} from '@/app/lib/placeholder-data';
 import Post from '@/app/ui/components/posts/Post';
-import {connectToDB} from '@/app/lib/data';
+import { connectToDB, getPosts } from '@/app/lib/data';
 
 export default async function Page() {
     const client = await connectToDB();
+    const posts = await getPosts();
     return (
         <>
             {client && <p className={'text-green-500'}>Connected to DB</p> }
             <h1 className={'text-sky-700'}>Posts</h1>
-            {posts.map((post) => <Post key={post.id} {...post} />)}
+            {posts?.map((post) => <Post key={post.id} {...post} />)}
         </>)
 }
--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -1,5 +1,5 @@
 import { neon } from '@neondatabase/serverless';
-import {createClient} from "@vercel/postgres";
+import {createClient, sql} from "@vercel/postgres";
 
 export async function connectToDB() {
     const client = createClient();
@@ -13,4 +13,15 @@ export async function connectToDB() {
     } catch (error) {
         console.error('Error connecting to DB', error);
     }
+}
+
+export async function getPosts() {
+    try {
+        const data = await sql`SELECT * FROM posts`
+        // console.log(data.rows)
+        return data.rows;    
+    } catch (error) {
+        console.log('Error getting posts', error)
+    }
+    
 }
