2cefa94686c23262620f1997cd569926d281a4d5
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-05-08 20:47:05 +0530

    Corrected some errors

--- auth.config.ts
+++ auth.config.ts
@@ -1,5 +1,8 @@
+import NextAuth from "next-auth";
 import type { NextAuthConfig } from 'next-auth';
 
 export const authConfig = {
     providers: [], // Add providers with an empty array for now
 } satisfies NextAuthConfig;
+
+export const { handlers, auth, signIn, signOut  } =  NextAuth(authConfig);
